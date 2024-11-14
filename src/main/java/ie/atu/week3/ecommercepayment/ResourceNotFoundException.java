9305f28930da327b96d6af0eb5871f8381d82b64
par2hibATU <parthibbdcox@gmail.com>
2024-11-14 20:39:08 +0000

    Error Handlers are added and working

---
+++ src/main/java/ie/atu/week3/ecommercepayment/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ie.atu.week3.ecommercepayment;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
